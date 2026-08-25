48b27467c746f735f50d0b8759a618dee07a4647
emreekiziltoprak <emre.kzltprkk@gmail.com>
2026-08-25 22:29:20 +0300

    fix: remove redundant initial domain folder

--- domain/EntryType.java
+++
@@ -1,6 +0,0 @@
-package domain;
-
-public enum EntryType {
-    DEBIT,
-    CREDIT
-}
--- domain/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public enum PaymentStatus {
-    INITIATED,
-    SUCCEEDED,
-    FAILED,
-    REFUNDED
-}
